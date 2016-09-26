9b1d4246621471a7fce280c064af3731e371c25e
irvan ariyanto <irvan.ariyanto104@gmail.com>
2016-09-25 17:44:57 -0700

    Create local IP Scanner

--- InternetAddressing/IpScanner/src/ipscanner/IpScanner.java
+++ InternetAddressing/IpScanner/src/ipscanner/IpScanner.java
@@ -5,6 +5,11 @@
  */
 package ipscanner;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
 /**
  *
  * @author Bay
@@ -14,8 +19,23 @@ public class IpScanner {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException, IOException {
         // TODO code application logic here
+        String ipAddress = InetAddress.getLocalHost().getHostAddress();
+        ipAddress = ipAddress.substring(0, ipAddress.lastIndexOf('.')) + ".";
+        for (int i = 0; i < 256; i++) {
+            String otherAddress = ipAddress + String.valueOf(i);
+            try {
+                if (InetAddress.getByName(otherAddress.toString()).isReachable(100)) {
+                    System.out.println(otherAddress + " is reachable");
+                } else {
+                    System.out.println(otherAddress + " is not reachable");
+                }
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
-    
 }
